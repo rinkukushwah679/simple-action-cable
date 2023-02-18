defd6e2b9829c0b14a88ee2cf96b87042f512b3a
Arvind Kushwah <arvindkushwah9@gmail.com>
2023-02-18 14:57:52 +0530

    Add action calbe

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -7,27 +7,8 @@ module ApplicationCable
 		# identified_by :current_user
 
 		def connect
-			# self.current_user = find_verified_user
 		end
 
 		private
-			# def find_verified_user
-			# 	token = request.headers[:token] || request.params[:token]
-			# 	if token
-			# 		verified_user = ::AccountBlock::Account.find_by(id: ::BuilderJsonWebToken.decode(token).id)
-			# 		#===============stock===================
-			# 		# if request.params["stock_ids"].present?
-			# 		# 	request.params["stock_ids"].join("").split(",").each do |stock_id|
-			# 		# 		BxBlockStockSubscription::StockSubscription.where(account_id: verified_user.id).destroy_all
-			# 		# 		stock_subscription = BxBlockStockSubscription::StockSubscription.new(stock_company_id: stock_id,account_id: verified_user.id)
-			# 		# 		stock_subscription.save
-			# 		# 	end
-			# 		# end
-			# 		verified_user
-			# 	else
-			# 		# verified_user = ::AccountBlock::Account.first
-			# 		reject_unauthorized_connection
-			# 	end
-			# end
 	end
 end
